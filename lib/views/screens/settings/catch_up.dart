62ffcea89553e1349a2d8820498424cedaf1d3c2
daveromer <davidromero9219@gmail.com>
2023-07-08 11:57:26 -0500

    Movie page Cathup

--- lib/views/screens/settings/catch_up.dart
+++ lib/views/screens/settings/catch_up.dart
@@ -1,6 +1,9 @@
 import 'package:app_movil_iptv/data/models/channel.dart';
+import 'package:app_movil_iptv/data/models/movies.dart';
 import 'package:app_movil_iptv/utils/consts.dart';
+import 'package:app_movil_iptv/viewmodels/movies_viewmodel.dart';
 import 'package:app_movil_iptv/viewmodels/tvlive_viewmodel.dart';
+import 'package:app_movil_iptv/views/screens/movies/movie_detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -15,19 +18,28 @@ class _CatchUpPageState extends State<CatchUpPage>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   TvLiveViewModel viewModelTvLive = TvLiveViewModel();
+  MoviesViewModel viewModelMovies = MoviesViewModel();
   late Future<List<ClsChannel>>? futureChannels;
+  late Future<List<ClsMovies>>? futureMovies;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 3, vsync: this);
     futureChannels = viewModelTvLive.allChannelsCatchUp();
+    futureMovies = viewModelMovies.allMoviesCatchUp();
   }
 
   @override
   void dispose() {
     _tabController.dispose();
     super.dispose();
+  }
+
+  void updateMovies() {
+    setState(() {
+      futureMovies = viewModelMovies.allMoviesCatchUp();
+    });
   }
 
   @override
@@ -72,7 +84,11 @@ class _CatchUpPageState extends State<CatchUpPage>
                 controller: _tabController,
                 children: [
                   CatchUpChannelsWidget(catchUpChannels: futureChannels!),
-                  const Center(child: Text('Favorite Movies')),
+                  CatchUpMoviesWidget(
+                    catchUpMovies: futureMovies!,
+                    moviesViewModel: viewModelMovies,
+                    onUpdate: updateMovies,
+                  ),
                   const Center(child: Text('Favorite Series')),
                 ],
               ),
@@ -166,6 +182,185 @@ class CatchUpChannelsWidget extends StatelessWidget {
                       ),
                       Text(
                         'No catch up channels found',
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ),
+                    ],
+                  );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class CatchUpMoviesWidget extends StatelessWidget {
+  final Future<List<ClsMovies>> catchUpMovies;
+  final MoviesViewModel moviesViewModel;
+  final VoidCallback onUpdate;
+
+  const CatchUpMoviesWidget({
+    Key? key,
+    required this.catchUpMovies,
+    required this.moviesViewModel,
+    required this.onUpdate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+      child: FutureBuilder<List<ClsMovies>>(
+        future: catchUpMovies,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error loading catch-up movies'),
+            );
+          } else {
+            var movies = snapshot.data;
+            return movies!.isNotEmpty
+                ? GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisExtent: 245,
+                      crossAxisCount: 5,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5,
+                    ),
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MovieDetailPage(
+                              clsMovies: movies[index],
+                            ),
+                          ),
+                        ).then((value) => onUpdate()),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            color: Const.colorPurpleDark.withOpacity(0.8),
+                            border: Border.all(
+                              color: Const.colorPurpleDarker,
+                              width: 1.0,
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              Stack(
+                                alignment: Alignment.topRight,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(8.0),
+                                      topRight: Radius.circular(8.0),
+                                    ),
+                                    child: FutureBuilder<String>(
+                                      future: moviesViewModel
+                                          .getMovieImage(movies[index]),
+                                      builder: (context, snapshot) {
+                                        if (snapshot.connectionState ==
+                                                ConnectionState.waiting ||
+                                            snapshot.data!.isEmpty) {
+                                          return Container(
+                                            height: 200.0,
+                                            width: 150.0,
+                                            color: Colors.grey,
+                                            child: ColorFiltered(
+                                              colorFilter:
+                                                  const ColorFilter.mode(
+                                                Colors.grey,
+                                                BlendMode.saturation,
+                                              ),
+                                              child: Image.asset(
+                                                Const.imgLogo,
+                                                fit: BoxFit.contain,
+                                              ),
+                                            ),
+                                          );
+                                        } else {
+                                          String imageUrl = snapshot.data!;
+                                          return FadeInImage.memoryNetwork(
+                                            placeholder: kTransparentImage,
+                                            image: imageUrl,
+                                            height: 200.0,
+                                            width: 150.0,
+                                            fit: BoxFit.cover,
+                                            fadeInDuration: const Duration(
+                                                milliseconds: 300),
+                                            fadeOutDuration: const Duration(
+                                                milliseconds: 300),
+                                            imageErrorBuilder:
+                                                (context, url, error) =>
+                                                    Container(
+                                              height: 200.0,
+                                              width: 150.0,
+                                              color: Colors.grey,
+                                              child: ColorFiltered(
+                                                colorFilter:
+                                                    const ColorFilter.mode(
+                                                  Colors.grey,
+                                                  BlendMode.saturation,
+                                                ),
+                                                child: Image.asset(
+                                                  Const.imgLogo,
+                                                  fit: BoxFit.contain,
+                                                ),
+                                              ),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: const BoxDecoration(
+                                      color: Const.colorPurpleMedium,
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(8.0),
+                                      ),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 4, vertical: 8),
+                                    child: Text(
+                                      movies[index]
+                                          .ratingMovie!
+                                          .toStringAsFixed(1),
+                                      style: Const.fontSubtitleTextStyle,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Text(
+                                movies[index].titleMovie!,
+                                textAlign: TextAlign.center,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: Const.fontBodyTextStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                : const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search_off,
+                        color: Colors.white,
+                        size: 50,
+                      ),
+                      Text(
+                        'No catch-up movies found',
                         style: TextStyle(fontSize: 25, color: Colors.white),
                       ),
                     ],
